d3ef76298e96dcefc19634af3df061ddd6e8d1b5
Rob Reynolds <ferventcoder@gmail.com>
2013-09-05 01:27:20 -0500

    Demo progress

--- Demo/puppet/manifests/site.pp
+++ Demo/puppet/manifests/site.pp
@@ -1,18 +1,46 @@
-
-# package {'name_of_package':
-#   provider => 'chocolatey',
-#   ensure => absent,latest, '1.0',
-#   source => 'http://not/default/sources;c:\local;\\some\network\share',
-#   install_options => '-installArgs "addtl args for native installer"',
-# }
+# default signifies all nodes (aka agents)
+node default {
 
 
-# # package {'putty':
-# #   ensure => latest,
-# #   provider => 'chocolatey',
-# # }
+  file { 'c:/temp':
+    ensure => 'directory',
+    notify => File['c:/temp/testfile.config'],
+  }  
 
-# package {'putty':
-#   ensure => absent,
-#   provider => 'chocolatey',
-# }
+  file { 'c:/temp/testfile.config':
+    ensure => 'file',
+    content => 
+'<config>
+</config>',
+  }
+
+  package {'roundhouse':
+    ensure => latest,
+    provider => chocolatey,
+    source => 'c:\vagrant\resources\packages',
+    #install_options => '-pre'
+  }
+
+  package {'vcredist2008':
+    ensure => latest,
+    provider => chocolatey,
+    notify => Reboot['reboot_vcredist'],
+  }
+
+  reboot { 'reboot_vcredist':
+    message => "Rebooting for Redist",
+    timeout => 5,
+  }
+
+  package { 'Microsoft SqlServer Express':
+    ensure => installed,
+    provider => windows,
+    source => 'c:/vagrant/resources/SQLServer/SQLEXPRWT_x64_ENU.exe',
+    require => Package['vcredist2008'],
+  } #->
+  # service { 'Sql Express'
+  #   ensure => 'running',
+  #   enable => true,
+  # }
+
+}
