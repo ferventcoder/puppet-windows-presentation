9300a86128b1d75ddf82dda78e912a6028b35c70
Rob Reynolds <ferventcoder@gmail.com>
2013-09-04 23:09:39 -0500

    adding Demo files

---
+++ Demo/puppet/manifests/site.pp
@@ -0,0 +1,37 @@
+
+# package {'name_of_package':
+#   provider => 'chocolatey',
+#   ensure => absent,latest, '1.0',
+#   source => 'http://not/default/sources;c:\local;\\some\network\share',
+#   install_options => '-installArgs "addtl args for native installer"',
+# }
+
+
+package {'git':
+  ensure => '1.8.1.2',
+  provider => chocolatey,
+  #install_options => '',
+  source => 'c:\vagrant\resources\packages;http://chocolatey.org/api/v2/',
+}
+
+# package {'git.install':
+#   ensure => latest,
+#   provider => 'chocolatey',
+#   source => 'c:\vagrant\resources\packages;http://chocolatey.org/api/v2/',
+# }
+
+package {'poshgit':
+  ensure => latest,
+  provider => 'chocolatey',
+  source => 'c:\vagrant\resources\packages;http://chocolatey.org/api/v2/',
+}
+
+# # package {'putty':
+# #   ensure => latest,
+# #   provider => 'chocolatey',
+# # }
+
+# package {'putty':
+#   ensure => absent,
+#   provider => 'chocolatey',
+# }
